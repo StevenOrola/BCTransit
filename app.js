2e8fa8e16b93802bcc24e6bb9eeeab75dcea8439
Steven Orola <stevenorola@gmail.com>
2018-07-29 16:54:56 -0700

    can display next 10 stops on localhost:3000

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ var express = require('express');
 var app = express();
 
 var Sequelize = require('sequelize');
-
+var Op = Sequelize.Op;
 var sequelize = new Sequelize('db', null, null, {
-    dialect: 'sqlite',
-    storage: './gtfs.sqlite'
+	dialect: 'sqlite',
+	operatorsAliases: Op,
+	logging: false,
+	storage: '../gtfs.sqlite'
 });
 
+var stops = sequelize.query("SELECT * FROM stop_times WHERE stop_id = 772 AND " +
+ "arrival_time > strftime('%H:%M:%S', 'now', 'localtime', '+3 minutes') " +
+ "ORDER BY arrival_time LIMIT 10;")
+/*
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  }, function (err) {
+    console.log('Unable to connect to the database:', err);
+  });*/
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send(stops);
 });
 
 app.listen(3000, () => {
